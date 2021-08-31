e9a5600a8d6018355006b98f893b76facff4a608
sdefreese <samanthadefreese1@gmail.com>
2021-08-30 18:41:07 -0700

    commit tree 44d3865573d7367fdccd120366590444c7878fed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31
+let num2=2
+let num3=54
+let num4=10
 
-// Write your code here
+let multiply = (num1 * num2); //value 62
+
+let random = (Math.floor(Math.random()*10) +1); //random number 0-1
+
+let mod = (num3 % num4);
+
+let max = Math.max(1, 2, 3, 4, 20);
